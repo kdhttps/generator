514eb90f4de78340607ec74acc9a8447b4e02867
kdhttps <kdhttps@gmail.com>
2020-06-04 21:02:15 +0530

    fix: fixed 404 issue

--- generator.sh
+++ generator.sh
@@ -3,6 +3,7 @@
 node_modules/@angular/cli/bin/ng build --prod
 
 cd dist/mdb-angular-free/
+cp index.html 404.html
 
 git init
 git config user.name kdhttps
@@ -10,5 +11,5 @@ git config user.email kdhttps@gmail.com
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com-kd:kdhttps/kdhttps.github.io.git master
+git push -f git@github.com:kdhttps/kdhttps.github.io.git master
 
